8c5e2883643b1f1caa19a5ba302f41137616099d
christiantepora <teporacj@gmail.com>
2021-06-24 16:50:10 +0800

    update images

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 // tl.to(".li-home", {opacity:"100%",y: "0%", duration:.2, stagger:0.25});
 // tl.to(".li-proj", {opacity:"100%",y: "0%", duration:.2, stagger:0.25});
 // tl.to(".li-contact", {opacity:"100%",y: "0%", duration:.2, stagger:0.25});
-tl.to(".left-col", {opacity:"100%",y: "0%", duration:1, stagger:0.25});
+tl.to(".left-col, .hero-img", {opacity:"100%",y: "0%", duration:1, stagger:0.5});
+tl.to(".logo,.li-home,.li-proj,.li-contact", {opacity:"100%",y: "0%", duration:.1, stagger:.1});
 
-tl.to(".hero-img, .logo", {opacity:"100%", duration:.5, stagger:0.25});
-tl.to(".li-home,.li-proj,.li-contact", {opacity:"100%",y: "0%", duration:.5, stagger:.1});
+
+
 console.log('hello')
